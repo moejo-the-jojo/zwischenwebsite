40b3d715416f5314963e5e898981c322207fb306
moejo-the-jojo <108475765+moejo-the-jojo@users.noreply.github.com>
2023-07-19 22:36:55 +0200

    more tests

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,11 @@ import { useNavigate } from "react-router-dom";
 
 const About = (props) => {
     const navigate = useNavigate();
-    
+
     return (
         <div>
             <h3>This is the page about us!</h3>
-            <button onClick={() => navigate("/about")}>About</button>
+            <button onClick={() => navigate(-1)}>Go back home!</button>
         </div>
     )
 }
