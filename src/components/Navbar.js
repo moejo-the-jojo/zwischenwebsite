11b08ab97a38a52134b5ff5263b850aa77e34a77
moejo-the-jojo <108475765+moejo-the-jojo@users.noreply.github.com>
2023-11-05 21:19:31 +0100

    tttt

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -423,6 +423,13 @@ const Navbar = () => {
     currentRotation.current = rotated;
   }, [rotated]);
 
+  const appHeight = () => {
+    const doc = document.documentElement;
+    doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+  };
+
+  window.addEventListener("resize", appHeight);
+
   return (
     <div id="navigationBar">
       <div style={{ position: "sticky" }}></div>
