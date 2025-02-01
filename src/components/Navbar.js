f8a2caaecf98bab1595cc4033b4f50f32b8fd07b
moejo-the-jojo <108475765+moejo-the-jojo@users.noreply.github.com>
2025-02-01 19:52:29 +0100

    trials

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -507,69 +507,9 @@ const Navbar = () => {
           Über uns
         </NavLink>
 
-        <div
-          id="showsDropdownRoot"
-          className="dropdown linkStyle"
-          onMouseDown={(event) => {
-            setDropDownShowsState((prev) => !prev);
-            if (dropDownShowsState === true) {
-              addDropdownClasses(event.target);
-
-              // let bottomMargin;
-              // addDropdownClasses(element.container, element.content);
-              // global[`${element.container}MobileStatus`] = true;
-              // bottomMargin =
-              //   element.container === "showsDropdownRoot" ? "15vh" : "10vh";
-              // // if (element.container === "showsDropdownRoot") {
-              // //   bottomMargin = "15vh";
-              // // } else {
-              // //   bottomMargin = "10vh";
-              // // }
-              // document
-              //   .getElementById(element.container)
-              //   .style.setProperty("margin-bottom", bottomMargin);
-
-              // console.log("i run");
-            }
-            //put mobile dropdown here??!
-          }}
-        >
-          Shows &#9662;
-          <div id="showsDropdown" className="dropdownContent">
-            <NavLink
-              to="/naechste-show"
-              className="linkStyle dropdownLink"
-              onClick={() => {
-                document
-                  .getElementById("showsDropdown")
-                  .classList.remove("showDropdownContent");
-              }}
-            >
-              <div>Nächste</div>
-            </NavLink>
-            <NavLink
-              to="/vergangene-shows"
-              className="linkStyle dropdownLink"
-              onClick={() => {
-                document
-                  .getElementById("showsDropdown")
-                  .classList.remove("showDropdownContent");
-              }}
-            >
-              Vergangene
-            </NavLink>
-            <NavLink
-              to="/calender"
-              className="linkStyle dropdownLink"
-              onClick={() => {
-                document
-                  .getElementById("showsDropdown")
-                  .classList.remove("showDropdownContent");
-              }}
-            >
-              Kalender
-            </NavLink>
-          </div>
+        <div id="showsDropdownRoot" className="dropdown linkStyle">
+          Shows &#9662
+          <div id="showsDropdown">blasd</div>
         </div>
 
         <div id="galerieDropdownRoot" className="dropdown linkStyle">
---
+++ src/components/backupShowsDropdown.js
@@ -0,0 +1,65 @@
+
+        // <div
+        //   id="showsDropdownRoot"
+        //   className="dropdown linkStyle"
+        //   onMouseDown={(event) => {
+        //     setDropDownShowsState((prev) => !prev);
+        //     if (dropDownShowsState === true) {
+        //       addDropdownClasses(event.target);
+
+        //       // let bottomMargin;
+        //       // addDropdownClasses(element.container, element.content);
+        //       // global[`${element.container}MobileStatus`] = true;
+        //       // bottomMargin =
+        //       //   element.container === "showsDropdownRoot" ? "15vh" : "10vh";
+        //       // // if (element.container === "showsDropdownRoot") {
+        //       // //   bottomMargin = "15vh";
+        //       // // } else {
+        //       // //   bottomMargin = "10vh";
+        //       // // }
+        //       // document
+        //       //   .getElementById(element.container)
+        //       //   .style.setProperty("margin-bottom", bottomMargin);
+
+        //       // console.log("i run");
+        //     }
+        //     //put mobile dropdown here??!
+        //   }}
+        // >
+        //   Shows &#9662;
+        //   <div id="showsDropdown" className="dropdownContent">
+        //     <NavLink
+        //       to="/naechste-show"
+        //       className="linkStyle dropdownLink"
+        //       onClick={() => {
+        //         document
+        //           .getElementById("showsDropdown")
+        //           .classList.remove("showDropdownContent");
+        //       }}
+        //     >
+        //       <div>Nächste</div>
+        //     </NavLink>
+        //     <NavLink
+        //       to="/vergangene-shows"
+        //       className="linkStyle dropdownLink"
+        //       onClick={() => {
+        //         document
+        //           .getElementById("showsDropdown")
+        //           .classList.remove("showDropdownContent");
+        //       }}
+        //     >
+        //       Vergangene
+        //     </NavLink>
+        //     <NavLink
+        //       to="/calender"
+        //       className="linkStyle dropdownLink"
+        //       onClick={() => {
+        //         document
+        //           .getElementById("showsDropdown")
+        //           .classList.remove("showDropdownContent");
+        //       }}
+        //     >
+        //       Kalender
+        //     </NavLink>
+        //   </div>
+        // </div>
