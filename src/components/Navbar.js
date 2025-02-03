5502ec51226bdae52935cf95dac4c39777d8d155
moejo-the-jojo <108475765+moejo-the-jojo@users.noreply.github.com>
2025-02-03 19:09:57 +0100

    started conditionally rendering

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -181,157 +181,8 @@ const Navbar = () => {
     return () => window.removeEventListener("resize", updateWindowSize);
   }, []);
 
-  const addDropdownClasses = (container, content) => {
-    const dropdown = document.getElementById(content);
-    if (container === "galerieDropdownRoot") {
-      dropdown.classList.add(
-        "showDropdownContent",
-        "showDropdownContentSmallSize"
-      );
-    } else {
-      dropdown.classList.add("showDropdownContent");
-    }
-  };
-
-  const removeDropdownClasses = (container, content) => {
-    const dropdown = document.getElementById(content);
-    dropdown.classList.remove(
-      "showDropdownContentSmallSize",
-      "showDropdownContent"
-    );
-    document
-      .getElementById(container)
-      .style.setProperty("margin-bottom", "1vh");
-  };
-
   const [dropDownShowsState, setDropDownShowsState] = React.useState(false);
   const [dropDownGalerieState, setDropDownGalerieState] = React.useState(false);
-
-  /*
-  if dropdownshowstate is false and mouseover => add dropdown - change state
-  and reverse
-  */
-
-  // handle add/remove droppdown classes
-  React.useEffect(() => {
-    eventListenersParentsArray.forEach((element) => {
-      // global[element.container] = () => {
-      //   addDropdownClasses(element.container, element.content);
-      // };
-      // global[`${element.container}remover`] = () => {
-      //   removeDropdownClasses(element.container, element.content);
-      // };
-      global[`${element.container}MobileStatus`] = false;
-      global[`${element.container}MobileToggle`] = () => {
-        console.log(global[`${element.container}MobileStatus`]);
-        console.log("yes im attached <3");
-        if (global[`${element.container}MobileStatus`] === false) {
-          let bottomMargin;
-          addDropdownClasses(element.container, element.content);
-          global[`${element.container}MobileStatus`] = true;
-          bottomMargin =
-            element.container === "showsDropdownRoot" ? "15vh" : "10vh";
-          // if (element.container === "showsDropdownRoot") {
-          //   bottomMargin = "15vh";
-          // } else {
-          //   bottomMargin = "10vh";
-          // }
-          document
-            .getElementById(element.container)
-            .style.setProperty("margin-bottom", bottomMargin);
-
-          console.log("i run");
-        } else {
-          removeDropdownClasses(element.container, element.content);
-          global[`${element.container}MobileStatus`] = false;
-        }
-      };
-    });
-  }, []);
-
-  const addDesktopEventListeners = () => {
-    eventListenersParentsArray.forEach((element) => {
-      const button = document.getElementById(element.container);
-      //function direkt hier??
-      //button.addEventListener("mouseover", global[element.container]);
-      button.addEventListener("mouseover", () => {
-        addDropdownClasses(element.container, element.content);
-      });
-
-      button.addEventListener("mouseout", () => {
-        removeDropdownClasses(element.container, element.content);
-      });
-      button.addEventListener("onclick", global[`${element.container}remover`]);
-    });
-    window.addEventListener("scroll", handleScroll);
-    // setDesktopListeners(true);
-  };
-
-  const removeDesktopEventListeners = () => {
-    eventListenersParentsArray.forEach((element) => {
-      const button = document.getElementById(element.container);
-      button.removeEventListener("mouseover", global[element.container]);
-      button.removeEventListener(
-        "mouseout",
-        global[`${element.container}remover`]
-      );
-      button.removeEventListener(
-        "onclick",
-        global[`${element.container}remover`]
-      );
-    });
-    window.removeEventListener("scroll", handleScroll);
-    // setDesktopListeners(false);
-  };
-
-  const addMobileEventListeners = () => {
-    eventListenersParentsArray.forEach((element) => {
-      const button = document.getElementById(element.container);
-      button.addEventListener(
-        "mousedown",
-        global[`${element.container}MobileToggle`]
-      );
-    });
-    // window.addEventListener("touchmove", handleScroll);
-    // setMobileListeners(true);
-  };
-
-  const removeMobileEventListeners = () => {
-    eventListenersParentsArray.forEach((element) => {
-      const button = document.getElementById(element.container);
-      button.removeEventListener(
-        "mousedown",
-        global[`${element.container}MobileToggle`]
-      );
-    });
-    window.removeEventListener("touchmove", handleScroll);
-    // setMobileListeners(false);
-  };
-
-  //handle add/remove event listeners
-  React.useEffect(() => {
-    // CHECK FOR DEVICE, NOT WIDTH
-    if (currentWindowWidth.current > 540) {
-      // document.body.style.setProperty("--dropdownLinkBGColor", "black");
-      console.log("i run at the start for");
-      addDesktopEventListeners();
-      return () => {
-        removeDesktopEventListeners();
-      };
-    } else {
-      // document.body.style.setProperty("--dropdownLinkBGColor", "#efcc00");
-      addMobileEventListeners();
-      // if (
-      //   document.getElementById("realRoutesContainer").clientHeight >
-      //   window.innerHeight
-      // ) {
-      //   window.addEventListener("touchmove", handleScroll);
-      // }
-      return () => {
-        removeMobileEventListeners();
-      };
-    }
-  }, []);
 
   // React.useEffect(() => {
   //   if (currentWindowWidth.current > 540 && desktopListeners.current !== true) {
@@ -404,8 +255,8 @@ const Navbar = () => {
     if (showMobileNav === false) {
       currentSettings = hideMobileNavigation;
       eventListenersParentsArray.forEach((element) => {
-        removeDropdownClasses(element.container, element.content);
-        global[`${element.container}MobileStatus`] = false;
+        // removeDropdownClasses(element.container, element.content);
+        // global[`${element.container}MobileStatus`] = false;
         console.log("i set the classes to nonnav");
       });
     } else {
@@ -507,13 +358,56 @@ const Navbar = () => {
           Über uns
         </NavLink>
 
-        <div id="showsDropdownRoot" className="dropdown linkStyle">
-          Shows &#9662
-          <div id="showsDropdown">blasd</div>
+        <div
+          id="showsDropdownRoot"
+          className="dropdown linkStyle"
+          onClick={() => {
+            dropDownShowsState
+              ? setDropDownShowsState(false)
+              : setDropDownShowsState(true);
+          }}
+          onMouseOver={() => setDropDownShowsState(true)}
+          onMouseOut={() => setDropDownShowsState(false)}
+        >
+          Shows {String.fromCharCode(0x25be)}
+          {dropDownShowsState && (
+            <div id="showsDropdown">
+              <NavLink
+                to="/naechste-show"
+                className="linkStyle dropdownLink"
+                onClick={(event) => {
+                  event.stopPropagation();
+                  setDropDownShowsState(false);
+                }}
+              >
+                Nächste
+              </NavLink>
+              <NavLink
+                to="/vergangene-shows"
+                className="linkStyle dropdownLink"
+                onClick={(event) => {
+                  event.stopPropagation();
+                  setDropDownShowsState(false);
+                }}
+              >
+                Vergangene
+              </NavLink>
+              <NavLink
+                to="/calender"
+                className="linkStyle dropdownLink"
+                onClick={(event) => {
+                  event.stopPropagation();
+                  setDropDownShowsState(false);
+                }}
+              >
+                Kalender
+              </NavLink>
+            </div>
+          )}
         </div>
 
         <div id="galerieDropdownRoot" className="dropdown linkStyle">
-          Galerie &#9662;
+          Galerie {String.fromCharCode(0x25be)}
           <div id="galerieDropdown" className="dropdownContent">
             <NavLink
               to="/fotos"
